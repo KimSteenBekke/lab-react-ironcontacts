b60ce0cd09a585139886e8320e4162b9cb12f126
KimSteenBekke <Kim.Steen.Bekke@lego.com>
2022-02-22 22:47:39 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import ContactData from "./contacts.json";
 
 function App() {
-  return (
+       const [contacts, setContacts] = useState(ContactData.slice(0, 5));
+  const deleteContact = (id) => {
+    let updatedArray = contacts.filter((eachContact) => {
+      return eachContact.id !== id;
+    });
+    setContacts([...updatedArray]);
+  };
+
+  const contactTable = contacts.map((eachContact) => {
+    let imageUrl = eachContact.pictureUrl;
+    return (
+      <tr key={eachContact.id}>
+        <td>
+          <img src={imageUrl} alt={eachContact.name} />
+        </td>
+        <td>{eachContact.name}</td>
+        <td>{eachContact.popularity}</td>
+        <td>{eachContact.wonOscar ? "🏆" : ""}</td>
+        <td>{eachContact.wonEmmy ? "🌟" : ""}</td>
+        <td>
+          <button
+            className="delete-btn"
+            onClick={() => deleteContact(eachContact.id)}
+          >
+            🗑️
+          </button>
+        </td>
+      </tr>
+    );
+  });
+
+  const randomContact = () => {
+    let contactArray = [];
+    let isInContacts = false;
+    for (let i = 0; i < ContactData.length; i++) {
+      for (let j = 0; j < contacts.length; j++) {
+        if (ContactData[i].id === contacts[j].id) {
+          isInContacts = true;
+        }
+      }
+      if (!isInContacts) {
+        contactArray.push(ContactData[i]);
+      }
+      isInContacts = false;
+    }
+
+    let newContact =
+      contactArray[
+        Math.floor(Math.random() * (ContactData.length - contacts.length))
+      ];
+    setContacts(contacts.concat(newContact));
+  };
+
+  const sortPopularity = () => {
+    let sortedArray = contacts.sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
+    setContacts([...sortedArray]);
+  };
+
+  const sortName = () => {
+    let sortedArray = contacts.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    setContacts([...sortedArray]);
+  };
+
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Iron contacts</h1>
+      <button onClick={randomContact}>Add Random Contact</button>
+      <button onClick={sortPopularity}>Sort by Popularity</button>
+      <button onClick={sortName}>Sort by Name</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>{contactTable}</tbody>
+      </table>
     </div>
   );
 }
